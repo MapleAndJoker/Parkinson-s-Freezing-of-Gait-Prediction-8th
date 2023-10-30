632ef95d3d7e5d14341535ccb53572ee751691dd
程程 冯 <1152817987@qq.com>
2023-10-30 16:53:54 +0800

    注释1

--- src/config.py
+++ src/config.py
@@ -20,11 +20,17 @@ class TrainGlobalConfig:
     window_size = 1000
     window_future = 50
     window_past = window_size - window_future
+    '''
+    滑动窗口是处理时序数据的常见方法 在时间序列数据中捕获局部模式，并为机器学习模型提供固定大小的输入
+    950 past→50 future
+    一个优点是提供了一种结构化的方法来捕获时序数据中的模式和依赖性
+    选择合适的窗口大小是关键。太小的窗口可能无法捕获足够的信息，而太大的窗口可能会导致计算上的挑战，并可能包括不必要的信息。
+    '''
 
     folds = 5
     num_workers = 4
     batch_size = 1024
-    n_epochs = 1
+    n_epochs = 1 #?
     lr = 0.001
 
     folder = "weights/FoG/"
@@ -38,3 +44,4 @@ class TrainGlobalConfig:
     scheduler_params = dict(
         mode="min", factor=0.7, patience=10, verbose=False, min_lr=0.000001
     )
+    #基于验证损失的学习率衰减
--- src/model.py
+++ src/model.py
@@ -6,12 +6,24 @@ The model has been taken from https://github.com/JanBrem/AI-NT-proBNP
 """
 
 
+'''
+计算在卷积操作中所需的填充（padding）大小，以便在给定步长（或降采样率）和卷积核大小的情况下，尽量使输出尺寸与输入尺寸接近或相同
+步长为 1 ，输出尺寸与输入尺寸相同
+当步长大于1时，即使使用了这个函数计算的填充值，输出尺寸仍然会小于输入尺寸
+'''
 def _padding(downsample, kernel_size):
     """Compute required padding"""
     padding = max(0, int(np.floor((kernel_size - downsample + 1) / 2)))
     return padding
 
 
+'''
+计算降采样率，并确保降采样是合理和有效的
+
+降采样是将信号或数据集的采样率降低的过程
+在时序数据、信号处理或图像处理中，降采样的主要目的是减少数据的数量，从而减少计算复杂性和存储需求，同时保留信号或数据集的主要特征。
+在降采样之前应用适当的滤波器可以减少混叠效应，保留数据的重要特征。
+'''
 def _downsample(n_samples_in, n_samples_out):
     """Compute downsample rate"""
     downsample = int(n_samples_in // n_samples_out)
@@ -36,13 +48,24 @@ class ResBlock1d(nn.Module):
         if kernel_size % 2 == 0:
             raise ValueError("This implementation only supports odd `kernel_size`.")
         super(ResBlock1d, self).__init__()
+        #super() 函数返回了 ResBlock1d 类的父类 即 nn.Module
 
         # Forward path
         padding = _padding(1, kernel_size)
         self.conv1 = nn.Conv1d(
             n_filters_in, n_filters_out, kernel_size, padding=padding, bias=False
-        )
+        )        
         self.bn1 = nn.BatchNorm1d(n_filters_out)
+        '''
+        当卷积层后面紧跟着BN层时，通常会选择不使用偏置
+        因为批量归一化层本身会有一个偏移项，与偏置项的功能重叠
+        '''
+
+        '''
+        例
+        Conv1d 将一个长度为 100 的一维信号转换成另一个长度可能不同的一维信号
+        而 Conv2d 将一个 28x28 的二维图像转换成另一个可能尺寸不同的二维图像
+        '''
         self.relu = nn.LeakyReLU()
         self.dropout1 = nn.Dropout(dropout_rate)
         padding = _padding(downsample, kernel_size)
@@ -54,15 +77,33 @@ class ResBlock1d(nn.Module):
             padding=padding,
             bias=False,
         )
+        #在卷积神经网络中，增加步长是实现降采样的常见方法
+
         self.bn2 = nn.BatchNorm1d(n_filters_out)
         self.dropout2 = nn.Dropout(dropout_rate)
 
+        '''
+        跳跃连接是一种常见的技巧，特别是在残差网络（ResNet）架构中。
+        跳跃连接的主要思想是直接将某一层的输出添加到更深层的输出，从而跳过一到多个层。这种连接方式可以帮助避免深度网络中的梯度消失和梯度爆炸问题，同时也能增强模型的表达能力。
+        例如，在一个简单的ResNet块中，输入首先通过一到两个卷积层进行处理，然后直接与块的输出相加，形成跳跃连接。
+
+        skip_connection_layers 列表可能被用于存储那些需要在后续层与其他层的输出进行合并的层的输出。
+        当网络向前传播时，我们可能会将某些层的输出保存到 skip_connection_layers 中。
+        然后，在后续的层中，我们可以从 skip_connection_layers 中取出这些输出，并与当前层的输出合并，实现跳跃连接。
+        '''
         # Skip connection
         skip_connection_layers = []
 
         # Deal with downsampling
         if downsample > 1:
             maxpool = nn.MaxPool1d(downsample, stride=downsample)
+            '''
+            确保输入特征的尺寸精确地减少一个因子 downsample
+            窗口为 downsample: 每个池化窗口都会覆盖 downsample 个连续的输入值。然后，从这些值中选取最大值作为池化操作的输出。
+            步长为 downsample: 每次移动池化窗口时，都会跳过 downsample 个输入值。这确保了每个池化窗口的输出都不重叠
+            因此输出尺寸确实减少了一个因子 downsample。
+            '''
+
             skip_connection_layers += [maxpool]
 
         # Deal with n_filters dimension increase
